7368924880afb54cfa10eafe41252b00e2a0f1de
Samuel Challe <samuel.challe@gmail.com>
2021-12-05 17:53:55 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
-# TODO : Erwann déconseille l’import from … import …
-from model.player import Player
-from model.table import Table
+import gui.epsi_poker_gui as gui
 
 
 def main():
-    player = Player("Human")
-    bot_first = Player("IA 1")
-    bot_second = Player("IA 2")
-    bot_third = Player("IA 3")
-
-    players = [player, bot_first, bot_second, bot_third]
-
-    table = Table(10)
-    table.first_draw(players)
-
-    table.bet(player, 8)
-    table.bet(bot_first, 3)
-    table.bet(bot_second, 5)
-    table.bet(bot_third, 12)
-
-    for player in players:
-        print(player)
-
-    print(table.get_cards())
+    # create the application
+    epsi_poker_gui = gui.EpsiPokerGui()
+    epsi_poker_gui.mainloop()
 
 
 if __name__ == "__main__":
