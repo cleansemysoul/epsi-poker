d197e54ff0ec8522202c004565fdecc60e60e213
Samuel Challe <samuel.challe@gmail.com>
2021-12-08 21:37:30 +0100

    update solo_game.py

--- model/solo_game.py
+++ model/solo_game.py
@@ -3,6 +3,7 @@ import model.hand as ha
 import model.human as hu
 import model.player as p
 import model.ai as a
+import model.table as t
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -12,7 +13,8 @@ class SoloGame:
 
     HUMAN_NAME = 'Vous'
     AI_NAME = ['Magali', 'Mégane', 'Enora']
-    NB_CARDS = 5
+    NB_CARDS_HAND = 2
+    NB_CARDS_TABLE = 3
 
     def __init__(self, seed: int):
 
@@ -32,6 +34,11 @@ class SoloGame:
             return ai
         self.__ai_players = list(map(add, self.AI_NAME))
 
+        # add table
+        table = t.Table()
+        self.__deck.move_cards_to_table(table, self.NB_CARDS_TABLE)
+        self.__table = table
+
     # a getter function
     @property
     def human_player(self):
@@ -42,8 +49,13 @@ class SoloGame:
     def ai_players(self):
         return self.__ai_players
 
+    # a getter function
+    @property
+    def table(self):
+        return self.__table
+
     def add_hand(self, player: p.Player):
         hand = ha.Hand()
         hand.sort_cards()
-        self.__deck.move_cards(hand, self.NB_CARDS)
+        self.__deck.move_cards_to_hand(hand, self.NB_CARDS_HAND)
         player.hand = hand
